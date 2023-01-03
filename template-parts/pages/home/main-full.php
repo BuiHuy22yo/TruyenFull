c9c941e3a4b55b97557e0c4b61d0040220c212f5
BuiHuy22yo <buihuy22yo@gmail.com>
2023-01-03 16:38:23 +0700

    update home

--- template-parts/pages/home/main-full.php
+++ template-parts/pages/home/main-full.php
@@ -9,21 +9,24 @@ $icon = CTWP_ICON_RIGHT;
 $url = home_url('truyen-full')
 ?>
 <div class="main-inner main-full">
-    <div class="row">
-        <div class="col-12">
-            <div class="inner">
-                <div class="title d-flex align-items-center">
-                    <a href="<?php echo $url ?>">
-                        <h2><?php echo esc_html__('Truyên Đã Hoàn thanh', CTWP_DOMAIN) ?></h2>
-                    </a>
-                    <span><?php echo $icon ?></span>
-                </div>
+    <div class="title-list">
+        <div class="inner">
+            <div class="title d-flex align-items-center">
+                <a href="<?php echo $url ?>">
+                    <h2><?php echo esc_html__('Truyên Đã Hoàn thanh', CTWP_DOMAIN) ?></h2>
+                </a>
+                <span><?php echo $icon ?></span>
             </div>
         </div>
-        <?php if ($data && is_array($data)) {
-            foreach ($data as $item) { ?>
-                <div class="col-4"></div>
-            <?php }
-        } ?>
     </div>
+    <?php if ($data && is_array($data)) { ?>
+        <div class="list">
+            <div class="row">
+                <?php foreach ($data as $item) { ?>
+                    <div class="col-4"></div>
+                <?php } ?>
+            </div>
+        </div>
+    <?php } ?>
 </div>
+
--- template-parts/pages/home/main-new-update.php
+++ template-parts/pages/home/main-new-update.php
@@ -9,20 +9,20 @@ $icon = CTWP_ICON_RIGHT;
 $url = home_url('truyen-moi')
 ?>
 <div class="main-inner main-new-update">
-    <div class="row">
-        <div class="col-12">
-            <div class="inner d-flex align-items-center justify-content-between">
-                <div class="title d-flex align-items-center">
-                    <a href="<?php echo $url ?>">
-                        <h2><?php echo esc_html__('Truyên Mới Cập Nhật', CTWP_DOMAIN) ?></h2>
-                    </a>
-                    <span><?php echo $icon ?></span>
-                </div>
-                <div class="filter-cat">
-                    <?php echo ctwp_html_categories() ?>
-                </div>
+    <div class="title-list">
+        <div class="inner d-flex align-items-center justify-content-between">
+            <div class="title d-flex align-items-center">
+                <a href="<?php echo $url ?>">
+                    <h2><?php echo esc_html__('Truyên Mới Cập Nhật', CTWP_DOMAIN) ?></h2>
+                </a>
+                <span><?php echo $icon ?></span>
+            </div>
+            <div class="filter-cat">
+                <?php echo ctwp_html_categories() ?>
             </div>
         </div>
+    </div>
+    <div class="row">
         <div class="col-8"></div>
         <div class="col-4">
             <?php get_sidebar(); ?>
